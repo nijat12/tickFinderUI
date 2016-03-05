5c210bb19e81c365e4664a73e60d5d6b010e5a92
Nijat Zeynalov <nijat12@gmail.com>
2016-03-05 10:02:32 -0500

    Yoman setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tickFinderUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tickFinderUiApp
+ */
+angular.module('tickFinderUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tickFinderUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tickFinderUiApp
+ */
+angular.module('tickFinderUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
