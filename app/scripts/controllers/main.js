a72f05f1aa2a08392383e89b7133c25ecc576934
Nijat Zeynalov <nijat12@gmail.com>
2016-03-05 23:02:09 -0500

    Media Querys

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,124 +7,128 @@
  * # MainCtrl
  * Controller of the tickFinderUiApp
  */
-tick.controller('MainCtrl', ['$scope', 'loadIcon', '$q', 'postServices', 'contactServices', '$uibModal', '$log',
-  function ($scope, loadIcon, $q, postServices, contactServices, $uibModal, $log) {
-  $scope.posts = [];
+tick.controller('MainCtrl', ['$scope', 'loadIcon', '$q', 'postServices', 'contactServices', '$uibModal',
+  function ($scope, loadIcon, $q, postServices, contactServices, $uibModal) {
+    $scope.posts = [];
+    $scope.contacts = [];
+    $scope.searchVal = '';
+    var modalInstance = null, modalInstance2 = null;
+
+    $scope.ok = function () {
+      modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+      modalInstance.close();
+    };
+
+    $scope.animationsEnabled = true;
+
+    $scope.open = function (size) {
+
+      modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: '../../views/modal/admin.html',
+        size: size,
+        scope:$scope
+      });
+    };
+
+    $scope.emailOpen = function (size, index) {
+
+      modalInstance2 = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: '../../views/modal/email.html',
+        size: size,
+        scope:$scope
+      });
+    };
+
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
 
 
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: '../../views/modal/admin.html',
-      controller: 'AdminCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-
-
-
-
-  $scope.getPosts = function (){
-    //var d = $q.defer();
+    $scope.getPosts = function () {
+      //var d = $q.defer();
 
       loadIcon.show();
-    postServices.getAll().then(function (data) {
+      postServices.getAll().then(function (data) {
         loadIcon.hide();
         //d.resolve(data);
-      $scope.posts = data;
+        $scope.posts = data;
       }, function (err) {
         loadIcon.hide();
         //d.reject(err);
       });
 
-    //return d.promise;
-  };
+      //return d.promise;
+    };
 
-  $scope.DeletePosts = function () {
-    //get id from which one to delete $index
-    loadIcon.show();
-    //for(var i = 50; i < 494; i++){
-    postServices.delete().then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
-  };
-
-
-
-
-
-  $scope.GetContacts = function (){
-    loadIcon.show();
-    contactServices.getAll().then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
-  };
-
-  $scope.DeleteContacts = function (){
-    loadIcon.show();
-    contactServices.deleteD(17).then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
-
-  };
+    $scope.DeletePosts = function (index) {
+      loadIcon.show();
+      //for(var i = 50; i < 494; i++){
+      postServices.delete($scope.posts[index].id).then(function () {
+        $scope.getPosts();
+        loadIcon.hide();
+      }, function (err) {
+        loadIcon.hide();
+      });
+    };
 
 
 
 
-  $scope.UpdateContacts1 = function (){
-    loadIcon.show();
-    contactServices.updateU({id:11, name:'Nijat11',email:'nijTestm',phone:'0005550'}).then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
 
-  };
 
-  $scope.create2 = function (){
-    loadIcon.show();
-    contactServices.createC().then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
+    //Contents
+    $scope.GetContacts = function () {
+      loadIcon.show();
+      contactServices.getAll().then(function (data) {
+        loadIcon.hide();
+        $scope.contacts = data;
+      }, function (err) {
+        loadIcon.hide();
+      });
+    };
 
-  };
+    $scope.DeleteContacts = function (index) {
+      loadIcon.show();
+      contactServices.deleteD($scope.contacts[index].id).then(function () {
+        loadIcon.hide();
+        $scope.GetContacts();
+      }, function (err) {
+        loadIcon.hide();
+      });
 
-  $scope.CreateContacts = function (){
-    loadIcon.show();
-    contactServices.createC({name:'Nijat11',email:'nijTestm',phone:'0005550'}).then(function () {
-      loadIcon.hide();
-    }, function (err) {
-      loadIcon.hide();
-    });
-  };
+    };
 
-  $scope.getPosts();
-}]);
+
+    $scope.UpdateContacts1 = function (index) {
+      console.log($scope.contacts[index]);
+      //loadIcon.show();
+      contactServices.updateU($scope.contacts[index]).then(function () {
+        //loadIcon.hide();
+      }, function (err) {
+        console.log(err);
+        //loadIcon.hide();
+      });
+
+    };
+
+
+    $scope.CreateContacts = function () {
+      loadIcon.show();
+      contactServices.createC().then(function (data) {
+        loadIcon.hide();
+        $scope.contacts.push(data);
+      }, function (err) {
+        loadIcon.hide();
+      });
+    };
+
+    $scope.getPosts();
+    $scope.GetContacts();
+
+    //$scope.open('lg');
+  }]);
--- app/views/modal/admin.js
+++ app/views/modal/admin.js
@@ -13,18 +13,5 @@
  */
 tick.controller('AdminCtrl', function ($scope, $uibModalInstance, items) {
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-
 
 });
